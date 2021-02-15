c3ffa654b141e92f42275eef1272d06030eb3a04
Egenaz Turemis <egenazturemis@gmail.com>
2021-02-15 16:35:10 -0500

    add hardcoded 'logged in as' to the right end of navbar

--- my-app/client/src/components/NavBar.js
+++ my-app/client/src/components/NavBar.js
@@ -15,6 +15,22 @@ export default function NavBar(props) {
         />{' '}
         WHALE MAIL
       </Navbar.Brand>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Mailbox</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">New Letter</a>
+        </li>
+      </ul>
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          Signed in as: <a href="#login">Mark Otto</a>
+        </Navbar.Text>
+      </Navbar.Collapse>
     </Navbar>
   </>
   )};
