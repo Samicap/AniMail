a0b05c1b491cefd8a27f8e2b94a77ad0671f87dc
samicap <lovestochasewindmills@gmail.com>
2021-02-15 17:08:46 -0500

    add React Routing to App.js

--- my-app/client/src/App.js
+++ my-app/client/src/App.js
@@ -1,13 +1,41 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-
+import Login from './components/Login';
+import PrivateComponent from './components/PrivateComponent';
 import Message from './components/Message';
+import useAppData from './hooks/useAppData'
+import { useState } from 'react';
+
+
 
 
 function App() {
+  // console.log("apple in App.js", useAppData)
+  const [state, setState] = useState({ loggedInUser: null})
+  // in logged out state
+  const loggedInUser = user => setState({ ...state, loggedInUser: user})
+
   return (
     <div className="App">
-      <Message />
+      <Router>
+        <Switch>
+          <Route exact path='/login'>
+            <Login/>
+            <PrivateComponent />
+          </Route>
+          <Route exact path='/messages'>
+
+            <Message />
+            <p>Apple</p>
+
+          </Route>
+          <Route path='*'>
+            <h1>404</h1>
+          </Route>
+        </Switch>
+
+      </Router>
     </div>
   );
 }
