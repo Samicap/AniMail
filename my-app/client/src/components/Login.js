e0e881ff73100241ec9a46e7d418df9cf638ba68
samicap <lovestochasewindmills@gmail.com>
2021-02-15 17:09:18 -0500

    add rudimentary login form and PrivateComponent for user login authentication

--- my-app/client/src/components/Login.js
+++ my-app/client/src/components/Login.js
@@ -1,4 +1,25 @@
-export default function Login(props) {
 
-  return <h1>I am Login</h1>
+
+
+export default function Login(props) {
+//event listener on submit on form.
+//when it catches creat an axios request that send the post to backend
+// axios.post('/login)
+  return (
+    <section>
+      <h1>PrivateComponent</h1>
+    <div>
+      <p>Login</p>
+      <h1>I am Login</h1>
+        <label>Username : </label>   
+        <input type="text" placeholder="Enter Username" name="username" required></input>
+        <label>Password : </label>   
+        <input type="password" placeholder="Enter Password" name="password" required></input> 
+        <button type="submit">Login</button>   
+
+    </div>
+    </section>
+
+
+  )
 }
