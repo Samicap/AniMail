8ecc59724d4d0ffde1a098cdb0c49a122597c40c
Egenaz Turemis <egenazturemis@gmail.com>
2021-02-16 13:03:15 -0500

    fix UsersListItem

--- my-app/client/src/App.js
+++ my-app/client/src/App.js
@@ -14,7 +14,9 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Message />
+      <CreateMessage />
+      {/* <UsersListItem/> */}
+      {/* <Message /> */}
     </div>
   );
 }
--- my-app/client/src/components/UsersListItem.js
+++ my-app/client/src/components/UsersListItem.js
@@ -4,15 +4,15 @@ import { Figure, FigureImage, FigureCaption } from 'react-bootstrap';
 
 export default function UsersListItem(props) {
   return (
-    <Figure>
-      <Figure.Image
-        width={171}
-        height={180}
-        alt="171x180"
-        src="/whale.png"
-      />
-      <Figure.Caption>
-        NAZ
-      </Figure.Caption>
-    </Figure>
+      <Figure>
+        <Figure.Image
+          width={171}
+          height={180}
+          alt="171x180"
+          src="/whale.png"
+        />
+        <Figure.Caption>
+          NAZ
+        </Figure.Caption>
+      </Figure>
 )};
