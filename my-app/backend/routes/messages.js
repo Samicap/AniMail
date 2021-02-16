647f661960089661c0e72db911f5e6018e39f8ac
analuciachin <ananime@gmail.com>
2021-02-16 19:50:45 +0000

    add /children/:id (post)

--- my-app/backend/routes/messages.js
+++ my-app/backend/routes/messages.js
@@ -1,19 +1,58 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/children/:id", (req, res) => {
     db.query(`SELECT * FROM messages WHERE child_id_to = $1;`, [req.params.id])
-      .then(data => {
-        console.log('data', data.rows);
+      .then((data) => {
+        console.log("data", data.rows);
         const messages = data.rows;
         res.json({ messages });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
+
+  router.post("/children/:id", (req, res) => {
+    //const time_sent = Date.now();
+    const child_id = req.params.id;
+    const child_id_to = req.body.child_id_to;
+    const message = req.body.message;
+    const animal_id = req.body.animal_id;
+
+    console.log("child_id_to ", child_id_to);
+    console.log("message ", message);
+    console.log("animal_id ", animal_id);
+    //console.log("time_sent ", time_sent);
+    console.log("child_id ", child_id);
+
+    if (child_id_to === "" || message === "" || animal_id === "") {
+      res.status(401).send("There are empty fields in the form.");
+      return;
+    }
+    return db
+      .query(
+        `
+        INSERT INTO messages (message, is_sent, child_id_to, child_id_from, animal_id, dateTime_sending)
+        VALUES ($1, $2, $3, $4, $5, NOW())
+        RETURNING *;
+      `,
+        [message, true, child_id_to, child_id, animal_id]
+      )
+      .then((data) => {
+        console.log("data", data.rows);
+        const message = data.rows;
+        res.json({ message });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.get("/:id/children/:id", (req, res) => {
+    console.log("req.params", req.params);
+  });
+
   return router;
 };
