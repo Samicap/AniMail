3b17b7c661f397c862094c32bc4e407482832495
samicap <lovestochasewindmills@gmail.com>
2021-02-17 15:49:49 -0500

    cleaned up code and routes. create setState to use state gloablly

--- my-app/client/src/App.js
+++ my-app/client/src/App.js
@@ -1,61 +1,69 @@
-// import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-
-import Message from './components/Message';
-import NavBar from "./components/NavBar";
-import CreateMessage from './components/CreateMessage';
-import { useState, useEffect } from 'react'
-import axios from 'axios';
-import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
-
-import LoginForm from './components/LoginForm';
-import ParentProfile from './components/ParentProfile';
-import Netflix from './components/Netflix';
-import Inbox from './components/Inbox';
+import LoginForm from "./components/LoginForm";
+import Netflix from "./components/Netflix";
 
 function App() {
   //const [user, setUser] = useState(null)
   //axios.get("/api/users").then(res => console.log(res.data));
 
-  const [user, setUser] = useState({});
-  const [error, setError] = useState("");
+  const [state, setState] = useState({ currentUser: null });
+  // , currentProfile: null, messages: null
 
-  const getUser = userInfo => {
-    console.log("QWQJWQW", userInfo)
-    setUser(userInfo)
-  }
+  const getUser = (userInfo) => {
+    //! sets the state of the current user in line 24
+    //! getUser passes the new state to the parent component (app.js) from the child(parentprofile)
+    //! this allows the state to be passed down to other children
+    console.log("QWQJWQW", userInfo);
+    setState({ ...state, currentUser: userInfo });
+  };
 
-  const [profile, setProfile] = useState({});
+  // const [profile, setProfile] = useState({}); // delete
+  // const setProfile = profileInfo => setState({...state, currentProfile: profileInfo})
+  // ==========
 
- 
+  // messages should be an array of objects.
+  // [{
+  // id: 1,
+  // content: Text,
+  // sender: 1,
+  // reciever: 2,
+  // date: 11/11/11
+  //}]
 
-  // useEffect(() => console.log(user.id), [user]);
+  //Filter fucntion in js
+  // All sent messages use filter to see all messages to revciever #2. can useEffect
 
-  //client route: /api/profiles/parents/:id
+  // useEffect(() => {
+  //   axios({url: "/messages", method: "get"}).then((res) => {
+  //     console.log(res.data)
+  //     setState({...state, messages: res.data})
+  //   })
+  // }, [])
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" render={() => 
-            <LoginForm getUser={getUser} />
-          } />
+          <Route
+            exact
+            path="/"
+            render={() => <LoginForm getUser={getUser} />}
+          />
 
-          <Route exact path="/profiles/parents/:id" render={() => 
-            <ParentProfile userId={user.id}/>
-          } />
-          <Route exact path="/netflix" render={() => 
-            <Netflix userId={user.id} setProfile={setProfile}/>
-          } />
-          <Route exact path="/inbox" render={() => 
-            <Inbox userId={user.id} profile={profile}/>
-          } />
-
+          <Route
+            path="/netflix"
+            render={() => <Netflix users={state.currentUser} />}
+          />
         </Switch>
-
       </Router>
     </div>
   );
