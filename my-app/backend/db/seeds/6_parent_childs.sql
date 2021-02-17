aa390e19a3343a3fa95477fca9467130aa3a082d
samicap <lovestochasewindmills@gmail.com>
2021-02-17 15:48:12 -0500

    add more data to DB

--- my-app/backend/db/seeds/6_parent_childs.sql
+++ my-app/backend/db/seeds/6_parent_childs.sql
@@ -1,2 +1,4 @@
 INSERT INTO parent_childs (parent_id, child_id) VALUES (1, 1);
 INSERT INTO parent_childs (parent_id, child_id) VALUES (2, 2);
+INSERT INTO parent_childs (parent_id, child_id) VALUES (1, 3);
+INSERT INTO parent_childs (parent_id, child_id) VALUES (1, 4);
