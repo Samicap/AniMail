a4d2557907a2b0cff501a9f4b6e955a91244e909
samicap <lovestochasewindmills@gmail.com>
2021-02-17 15:44:11 -0500

    remove Parent Profile and made Netflix recieve data to chose users

--- my-app/client/src/components/Netflix.js
+++ my-app/client/src/components/Netflix.js
@@ -1,36 +1,19 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
 
-export default function Netflix({ userId }) {
+export default function Netflix({ users }) {
+  console.log("parentProfile", users);
   const [parentProfile, setParentProfile] = useState({});
-
-  useEffect(() => {
-    axios
-      .get(`/api/profiles/parents/${userId}`)
-      // new route that returns all rpofiles associated with account
-      // display those and user choses onemptie
-      // when they chose call setProfile
-      .then(function(response) {
-        console.log(response.data.parents[0])
-        const data = response.data.parents[0]
-        setParentProfile({data});
-      });
-  }, []);
 
   return (
     <div>
-       <Link to={'/inbox'}>Go to Netflix</Link>
-      <h1>Parent</h1>
-      {/*<p>{JSON.stringify(parentProfile.data)}</p>*/}
-      {parentProfile.data &&
-      <div>
-        <p>Parent ID: {parentProfile.data.parents_id}</p> 
-        <p>Parent Avatar URL: {parentProfile.data.parents_avatar_url}</p>
-        <p>Child Username: {parentProfile.data.childs_username}</p>
-        <p>Child Avatar URL: {parentProfile.data.childs_avatar_url}</p>
-      </div>
-      }
+      <h1>Netflix Choose User Page</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.childs_username}>
+            <button>{user.childs_username}</button>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
