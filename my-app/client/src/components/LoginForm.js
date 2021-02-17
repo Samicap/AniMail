539dc02516ff0aad798c96ca62456325fd874f0a
samicap <lovestochasewindmills@gmail.com>
2021-02-17 15:44:50 -0500

    change LoginForm to change the global state of the app.js

--- my-app/client/src/components/LoginForm.js
+++ my-app/client/src/components/LoginForm.js
@@ -1,63 +1,64 @@
-import axios from 'axios';
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export default function Login({ getUser }) {
-
-  const [parentInfo, setParentInfo] = useState({ id: "", username: "", email: "", avatar_url: "", password: "" });
-  const [loginInfo, setLoginInfo] = useState({ email: "", password: ""});
+  const [parentInfo, setParentInfo] = useState([]);
+  const [loginInfo, setLoginInfo] = useState({ email: "", password: "" });
 
   let history = useHistory();
 
-  const submitHandler = async (event) => { 
+  const submitHandler = (event) => {
     event.preventDefault();
 
-    await login(loginInfo);
-    console.log("parent info", parentInfo)
-    getUser(parentInfo)
-  }
+    login(loginInfo);
+  };
 
-
-  const login = loginInfo => {
-    axios.post("/api/login", {
-      email: loginInfo.email,
-      password: loginInfo.password
-    })
-    .then(function (response) {
-      console.log(response.data.parent[0]);
-      const data = response.data.parent[0];
-
-      getUser(data);
-      history.push(`/profiles/parents/${data.id}`)
-      // need to change /netflix
-      
-      setParentInfo({
-        id: data.id,
-        username: data.username,
-        email: data.email,
-        avatar_url: data.avatar_url,
-        password: data.password
+  const login = (loginInfo) => {
+    axios
+      .post("/api/login", {
+        email: loginInfo.email,
+        password: loginInfo.password,
       })
+      .then(function (response) {
+        console.log(response.data.parent);
+        const data = response.data.parent;
 
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-  }
-  
+        getUser(data);
+        //! function call
+        setParentInfo(data);
+
+        history.push(`/netflix`);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <section>
       <h1>Login</h1>
       <form onSubmit={submitHandler}>
         <label htmlFor="name">Email</label>
-        <input type="text" name="email" onChange={event => setLoginInfo({...loginInfo, email: event.target.value})} value={loginInfo.email} />
+        <input
+          type="text"
+          name="email"
+          onChange={(event) =>
+            setLoginInfo({ ...loginInfo, email: event.target.value })
+          }
+          value={loginInfo.email}
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" name="email" onChange={event => setLoginInfo({...loginInfo, password: event.target.value})} value={loginInfo.password} />
+        <input
+          type="password"
+          name="email"
+          onChange={(event) =>
+            setLoginInfo({ ...loginInfo, password: event.target.value })
+          }
+          value={loginInfo.password}
+        />
         <input type="submit" value="Login" />
-
       </form>
     </section>
-  )
-
+  );
 }
