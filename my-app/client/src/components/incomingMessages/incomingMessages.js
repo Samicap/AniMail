cfc101c9ce50ce939cbc74818436dd69afe79cd8
samicap <lovestochasewindmills@gmail.com>
2021-02-18 13:53:33 -0500

    add progress bar that updates dynamically based on speed passed in throguh app.js

--- my-app/client/src/components/incomingMessages/incomingMessages.js
+++ my-app/client/src/components/incomingMessages/incomingMessages.js
@@ -1,34 +1,35 @@
 import { useState, useEffect } from 'react';
-import ProgressBarApple from '../progressBar/ProgressBar';
-
+import ProgressBar from '../progressBar/ProgressBar';
 
 export default function IncomingMessage(props) {
 
+  const { avatar, speed } = props;
   //! Props need to have the animal chosen and the speed
   //! expecting this to be an object {animal}
-
-  const animal = {
-    name: 'Zebra',
-    avatar: 'https://www.flaticon.com/svg/vstatic/svg/714/714011.svg?token=exp=1613346041~hmac=87dc8721fd0a92c8b4cb0284fbcb199d',
-    speed: 65,
-  }
+//  console.log("animal", animal)
   
+ //! do i need to put this useEffect inside a function?
   const [value, updateValue] = useState(0);
-
   useEffect(() => {
     const interval = setInterval(() => {
       updateValue(oldValue => {
-        const newValue = oldValue + 10;
+        const newValue = oldValue + speed
+        if (newValue >= 100) {
+          clearInterval(interval)
+        }
         return newValue;
       })
-    }, 1000)
+    }, 50)
+    return () => clearInterval(interval);
   }, []);
-
+  
+  //   //! the 1000 number is what is changes based on animal type?
 
   return (
     <div>
       <h1>I am an Incoming Message</h1>
-      <ProgressBarApple value={value} />
+      <img src="{avatar}" ></img>
+      <ProgressBar value={value} />
       <p>Animal Avatar Based on What user selected from Create Message</p>
       <p>Progress bar based on the speed of the animal provided from the DB</p>
       <div >
