e7124255e8550414598865dfbffac37156fa82bb
ThomasA64 <thomas.arthur64@gmail.com>
2021-02-18 18:51:30 -0500

    brought in Child component code

--- my-app/client/src/components/Child.js
+++ my-app/client/src/components/Child.js
@@ -1,4 +1,14 @@
 export default function Child({childId}) {
 
-  return <h1>I am {childId}</h1>
+  return  ( <div class="card">
+  <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiMlAbzhA9a-By_ekwI66JAToCZZFkU8jTZA&usqp=CAU" alt="Card image cap"/>
+
+  <div class="card-body">
+        childs_username , childs_age
+  </div>
+  <div class="card-body">
+    childs_location_id
+  </div>
+</div>
+ )
 }
