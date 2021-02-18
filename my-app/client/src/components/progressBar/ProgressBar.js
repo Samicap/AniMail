2dfb4c7ae0a8a895d9b326dec975a5bbc60e21e5
samicap <lovestochasewindmills@gmail.com>
2021-02-18 11:58:28 -0500

    add a progress bar using HTML

---
+++ my-app/client/src/components/progressBar/ProgressBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {useEffect, useState} from 'react';
+import "react-step-progress-bar/styles.css";
+import "./progressBar.css";
+import { ProgressBar } from "react-step-progress-bar";
+
+// export default class ProgressBarApple extends React.Component {
+//   render() {
+//     return (
+//       <ProgressBar
+//         percent={75}
+//         filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+//       />
+//     );
+//   }
+// }
+
+//! Props must be a number!  the animal speed
+// export default function ProgressBarApple(props) {
+//   return (
+//     <ProgressBar
+//       percent={75}
+//       filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+//     />
+//   )
+// }
+
+export default function ProgressBarApple(props) {
+  const { value, max } = props;
+  return <progress value={value} max="100" />;
+}
