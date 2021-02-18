63ae11d06ff17e8cb20a3ec82ad1d737fb13176d
Egenaz <egenazturemis@gmail.com>
2021-02-18 01:14:49 +0000

    create message css and import in message js

--- my-app/client/src/components/Message.js
+++ my-app/client/src/components/Message.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Bootstrap from "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from "react";
 import axios from "axios";
+
+import "components/InterviewerListItem.scss";
+
 
 export default function Message(props) {
 
@@ -11,8 +15,9 @@ export default function Message(props) {
 
 /*
 TODO: props needed: 
+
 messages table: child_id_from; dateTime_receiving; animal_id; message
 data to pull: child name, age, location; animal avatar; message
-*/
 
-//? What do we need to do to switch between the two states of a message: 1- collapsed, 2- expanded
+? What do we need to do to switch between the two states of a message: 1- collapsed, 2- expanded
+*/
