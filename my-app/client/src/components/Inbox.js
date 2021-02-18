b60310547971ca2addc05c44a461abfeed1d1f96
samicap <lovestochasewindmills@gmail.com>
2021-02-18 18:21:41 -0500

    add incoming messages component

---
+++ my-app/client/src/components/Inbox.js
@@ -0,0 +1,16 @@
+import IncomingMessages from "./incomingMessages/incomingMessages";
+
+
+
+export default function Inbox(props) {
+  const { childId, avatar, speed } = props;
+
+    if (is_recieved === false) {
+      
+    }
+
+  return (
+
+    <IncomingMessages avatar={avatar} speed={speed}/>
+  )
+ }
