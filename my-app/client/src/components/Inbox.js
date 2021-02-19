a23bffeb08bf7767333c41740a1038b3b9dba057
samicap <lovestochasewindmills@gmail.com>
2021-02-18 22:58:25 -0500

    modies setThisChildMessages to return response data for messages and animal info

--- my-app/client/src/components/Inbox.js
+++ my-app/client/src/components/Inbox.js
@@ -6,20 +6,21 @@ import IncomingMessages from "./incomingMessages/incomingMessages";
 
 export default function Inbox({ childId }) {
   // const { childId, avatar, speed } = props;
-  console.log("CHILD ID >>> ", childId);
+  // console.log("CHILD ID >>> ", childId);
 
   const [thisChildMessages, setThisChildMessages] = useState(null);
 
   useEffect(() => {
     axios.get(`/api/messages/children/${childId}`).then((response) => {
-      console.log("MESSAGES >>> ", response.data["messages"]); // returns an array of message objects (containing message and animal info)
+      setThisChildMessages(response.data["messages"]);
+      // console.log("MESSAGES >>> ", response.data["messages"]); // returns an array of message objects (containing message and animal info)
     });
   }, [childId]);
 
   return (
     <div>
       <p>INBOX</p>
-      {/* <IncomingMessages avatar={avatar} speed={speed}/> */}
+      <IncomingMessages childMessages={thisChildMessages} />
       <MessageList />
     </div>
   );
