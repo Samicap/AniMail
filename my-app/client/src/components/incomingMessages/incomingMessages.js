dbfdabf826bfc8090a1375f6028838ccfa311fe8
samicap <lovestochasewindmills@gmail.com>
2021-02-18 23:00:24 -0500

    filter over data passed by inbox to filter out un-recieved messages and to pass avatar and speed to progressbar component

--- my-app/client/src/components/incomingMessages/incomingMessages.js
+++ my-app/client/src/components/incomingMessages/incomingMessages.js
@@ -1,54 +1,65 @@
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 import ProgressBar from "../progressBar/ProgressBar";
 
-// {
-//   "messages": [
-//   {
-//   "id": 1,
-//   "message": "Hello friend!",
-//   "is_sent": true,
-//   "is_received": true,
-//   "is_read": true,
-//   "duration": null,
-//   "datetime_sending": null,
-//   "datetime_delivering": null,
-//   "datetime_receiving": null,
-//   "child_id_to": 1,
-//   "child_id_from": 2,
-//   "animal_id": 2
-//   }
-//   ]
-//   }
 
-export default function IncomingMessage(props) {
-  console.log("oahgioasdjfsdjfi", props);
-  const { avatar, speed } = props;
+
+export default function IncomingMessage({ childMessages }) {
+  console.log("oahgioasdjfsdjfi", childMessages);
+  // const { avatar, speed } = props;
   //! Props need to have the animal chosen and the speed
   //! expecting this to be an object {animal}
 
-  // this useEffect is what makes the progress bar update dynamically. The speed is taken from the animal speed.
-  const [value, updateValue] = useState(0);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      updateValue((oldValue) => {
-        const newValue = oldValue + speed;
-        if (newValue >= 100) {
-          clearInterval(interval);
+  const [currentAnimalAvatar, setCurrentAnimalAvatar] = useState({avatar: null})
+  
+  const appleBABY = (childMessages) => {
+    return childMessages && childMessages.map(child => {
+      if (!child.is_recieved) {
+        return {
+        avatar: child.avatar_url,
+        speed: child.speed
         }
-        return newValue;
-      });
-    }, 50);
-    return () => clearInterval(interval);
-  }, []);
+      }
+    })
+  }
+
+  const animalData = appleBABY(childMessages);
+
+  
+  // this useEffect is what makes the progress bar update dynamically. The speed is taken from the animal speed.
+  // const [value, updateValue] = useState(0);
+
+  // useEffect(() => {
+  //   return animalData.map(animal => { 
+ 
+  //     const interval = setInterval(() => {
+  //       updateValue((oldValue) => {
+  //         const newValue = oldValue + animal.speed;
+  //         if (newValue >= 100) {
+  //           clearInterval(interval);
+  //         }
+  //         return newValue;
+  //       });
+  //     }, 50);
+  //     return () => clearInterval(interval);
+  //   })
+  //   }, []);
 
   //   //! the 1000 number is what is changes based on animal type?
 
   return (
     <div>
+      
       <h1>I am an Incoming Message</h1>
-      <img src={avatar} />
-      <ProgressBar value={value} />
+        {animalData && animalData.map(animal => {
+          return (
+            <div>
+              <img src={animal.avatar} />
+              <ProgressBar speed={.2} />
+            </div>
+          )
+        }) }
     </div>
   );
 }
