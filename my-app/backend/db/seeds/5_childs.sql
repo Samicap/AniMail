d008cf6a74ce7b6522474f84c517c02fcad1f50c
Egenaz <egenazturemis@gmail.com>
2021-02-20 19:21:38 +0000

    add assign new location ids for the sake of variety

--- my-app/backend/db/seeds/5_childs.sql
+++ my-app/backend/db/seeds/5_childs.sql
@@ -1,4 +1,4 @@
 INSERT INTO childs (username, avatar_url, age, language_id, location_id) VALUES ('Naz', '/kids-avatar-011', 8, 1, 1);
-INSERT INTO childs (username, avatar_url, age, language_id, location_id) VALUES ('Sam', '/kids-avatar-005', 8, 1, 2);
-INSERT INTO childs (username, avatar_url, age, language_id, location_id) VALUES ('Thomas', '/kids-avatar-018', 8, 1, 2);
-INSERT INTO childs (username, avatar_url, age, language_id, location_id) VALUES ('Ana', '/kids-avatar-012', 8, 1, 2);
+INSERT INTO childs (username, avatar_url, age, language_id, location_id) VALUES ('Sam', '/kids-avatar-005', 8, 1, 3);
+INSERT INTO childs (username, avatar_url, age, language_id, location_id) VALUES ('Thomas', '/kids-avatar-018', 8, 1, 4);
+INSERT INTO childs (username, avatar_url, age, language_id, location_id) VALUES ('Ana', '/kids-avatar-012', 8, 1, 5);
