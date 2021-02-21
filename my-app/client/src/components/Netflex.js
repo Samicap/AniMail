3f2328a73d8d7b62ad19a6f15d2ae3222baf9951
ThomasA64 <thomas.arthur64@gmail.com>
2021-02-20 19:26:56 -0500

    added NetFlex

---
+++ my-app/client/src/components/Netflex.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Netflex() {
+  return (
+    <>
+      <div class="wrapper">
+        <h1>Netflix User Page</h1>
+        <div class="profile-wrap">
+          <div class="profile">
+            <div class="profile-icon profile1"></div>
+            <div class="profile-name">Pays For Account Never Watches</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
